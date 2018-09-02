18a63ab7d55aaaf304041ee36f3a76e769dc1be2
Luis Perichon <luisperichon@gmail.com>
2018-09-02 13:05:44 -0300

    list tweets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TwitterService } from './twitter.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	constructor(private twitter: TwitterService) {}
+
   title = 'NowPlaying';
+  tweets = [];
+
+  ngOnInit() { 
+  	this.twitter.getTweets().subscribe(twitterResponse => {
+  		console.log(twitterResponse.data.statuses)
+  		this.tweets = twitterResponse.data.statuses
+	  });
+  }
 }
